2846ef5a52ef6393de657c6f1635701c7da0d340
MohamedFathi212 <mf974476@gmail.com>
2024-04-23 22:20:14 +0200

    first

---
+++ BaseInput.php
@@ -0,0 +1,26 @@
+<?php 
+
+require_once 'HtmlElement.php'; 
+
+abstract class BaseInput extends HtmlElement {
+    public string $label;
+    public string $name;
+    public string $value;
+
+    public function __construct(string $name, string $label = '', string $value = '')
+    {
+        $this->label = $label;
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function render(): string
+    {
+        return sprintf('<div class="form-group">
+                    <label>%s</label><br>
+                    %s
+            </div>', $this->label, $this->renderInput());
+    }
+
+    abstract public function renderInput(): string;
+}
